0772056d8dd3e0ed0d59658d3543015ae1911cea
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-11-01 22:12:25 -0500

    added problem description to java eof and started java initializer block problem

---
+++ src/main/java/JavaStaticInitializerBlock.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class JavaStaticInitializerBlock {
+    public static void main(String[] args) {
+
+        // First, let's initialize our Scanner Object to check for our inputs:
+        Scanner scanner = new Scanner(System.in);
+    }
+}
