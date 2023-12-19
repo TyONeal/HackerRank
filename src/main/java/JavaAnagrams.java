452c7426dfae690407926e04f51cf30e16bb832c
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-12-19 07:03:41 -0600

    started anagrams

---
+++ src/main/java/JavaAnagrams.java
@@ -0,0 +1,15 @@
+public class JavaAnagrams {
+
+    public boolean isSameFrequency(String a, String b) {
+        int frequency = 0;
+
+        for (int i = 0; i < a.length() - 1; i++) {
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
