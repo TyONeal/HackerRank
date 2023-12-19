f4c2ec35165080b4fbefe7eeec2a2399f55bbd6b
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-12-18 23:35:23 -0600

    working through comparisons problem

--- src/main/java/JavaSubstringComparisons.java
+++ src/main/java/JavaSubstringComparisons.java
@@ -9,13 +9,25 @@ public class JavaSubstringComparisons {
 
         String s = scanner.nextLine();
         int k = scanner.nextInt();
+        String solution = "";
 
         List<String> ourSubstrings = new ArrayList<>();
 
-        for (int i = 0; i < s.length() - k; i += 1) {
+        for ( int i = 0; i < s.length() - (k - 1); i ++ ) {
             String parsedString = s.substring(i , i + k);
-            ourSubstrings.add(parsedString);
+            ourSubstrings.add( parsedString );
         }
-        System.out.println(ourSubstrings);
+
+        System.out.println("substrings --->" + ourSubstrings);
+
+        for(int i = 0; i < ourSubstrings.size() - 1; i++) {
+            int compareValue = ourSubstrings.get(i).compareTo(ourSubstrings.get(i + 1));
+            if(compareValue < 0) {
+                solution = ourSubstrings.get(i);
+            }
+        }
+
+        System.out.println(solution);
+
     }
 }
