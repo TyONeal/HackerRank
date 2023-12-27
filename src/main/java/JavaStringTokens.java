694258b05bf701444d0e149b9594310cc7e8193e
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-12-26 21:06:17 -0600

    working through split method for token problem

--- src/main/java/JavaStringTokens.java
+++ src/main/java/JavaStringTokens.java
@@ -1,9 +1,16 @@
+import java.util.Arrays;
+
 public class JavaStringTokens {
 
     public static void getTokens(String s) {
         // Create custom regex for solving problem
-
+        String[]tokens = s.split(" (?=[A-Za-z]{2}:)");
         // Run split to get proper info.
+        System.out.println(Arrays.toString(tokens));
 
+    }
+
+    public static void main(String[] args) {
+        getTokens("Hey the're how are you");
     }
 }
