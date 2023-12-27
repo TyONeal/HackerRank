1b7124922de62999421fe0d6cf3282da8ece94b2
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-12-26 22:36:57 -0600

    built small services for starting regex

--- src/main/java/JavaStringTokens.java
+++ src/main/java/JavaStringTokens.java
@@ -1,16 +1,27 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class JavaStringTokens {
 
-    public static void getTokens(String s) {
-        // Create custom regex for solving problem
-        String[]tokens = s.split(" (?=[A-Za-z]{2}:)");
-        // Run split to get proper info.
-        System.out.println(Arrays.toString(tokens));
+    public static String[] getTokens(String input) {
+        return input.split("[' ?!.]");
+    }
 
+    public static long getTokenCount(String[]tokens) {
+        return Arrays.stream(tokens).count();
+    }
+
+    public static void displayTokens(String[]tokens) {
+        for(int i = 0; i < Arrays.stream(tokens).count(); i++) {
+            System.out.println(tokens[i].replace(",", "").trim());
+        }
     }
 
     public static void main(String[] args) {
-        getTokens("Hey the're how are you");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[]s = getTokens(input);
+        System.out.println(getTokenCount(s));
+        displayTokens(s);
     }
 }
