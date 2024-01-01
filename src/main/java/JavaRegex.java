1da65fc7ff761fed81226028da3dbb845e28c20b
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-12-31 18:05:05 -0600

    started regex problem

--- src/main/java/JavaRegex.java
+++ src/main/java/JavaRegex.java
@@ -1,9 +1,26 @@
-public class JavaRegex {
-    public static void checkRegex() {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class JavaRegex {
+
+    private static final String ipRegex = "^((0|1\\\\d?\\\\d?|2[0-4]?\\\\d?|25[0-5]?|[3-9]\\\\d?)\\\\.){3}(0|1\\\\d?\\\\d?|2[0-4]?\\\\d?|25[0-5]?|[3-9]\\\\d?)$";
+
+    private static final Pattern pattern = Pattern.compile(ipRegex);
+
+    private static void checkRegex(String s) {
+        Matcher matcher = pattern.matcher(s);
+        if(matcher.matches()) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 
     public static void main(String[] args) {
-        Scanner
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.hasNextLine()) {
+            checkRegex(scanner.nextLine());
+        }
     }
 }
