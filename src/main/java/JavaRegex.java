cff1403662c6cd97fa768a1814ec1a269138d4b8
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2024-01-01 18:13:08 -0600

    working on regex for problem

--- src/main/java/JavaRegex.java
+++ src/main/java/JavaRegex.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class JavaRegex {
 
-    private static final String ipRegex = "^((0|1\\\\d?\\\\d?|2[0-4]?\\\\d?|25[0-5]?|[3-9]\\\\d?)\\\\.){3}(0|1\\\\d?\\\\d?|2[0-4]?\\\\d?|25[0-5]?|[3-9]\\\\d?)$";
+    private static final String ipRegex = "^((25[0-5]|(2[0-4]|1\\\\d|[1-9]|)\\\\d)\\\\.?\\\\b){4}$";
 
     private static final Pattern pattern = Pattern.compile(ipRegex);
 
