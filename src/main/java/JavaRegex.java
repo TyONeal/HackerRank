9566acfe1332551e92568a1df3018c128900be36
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2024-01-02 21:17:05 -0600

    added notes to problem

--- src/main/java/JavaRegex.java
+++ src/main/java/JavaRegex.java
@@ -4,10 +4,13 @@ import java.util.regex.Pattern;
 
 public class JavaRegex {
 
+    // First, we establish our regex by creating our custom string:
     private static final String ipRegex = "^((25[0-5]|(2[0-4]|1\\\\d|[1-9]|)\\\\d)\\\\.?\\\\b){4}$";
 
+    // Next, we instantiate our pattern to compile our regex for comparison:
     private static final Pattern pattern = Pattern.compile(ipRegex);
 
+    // Here we check our string input against our previously created pattern:
     private static void checkRegex(String s) {
         Matcher matcher = pattern.matcher(s);
         if(matcher.matches()) {
@@ -17,6 +20,7 @@ public class JavaRegex {
         }
     }
 
+    // Finally, we run our main method to get our appropriate output:
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         if(scanner.hasNextLine()) {
